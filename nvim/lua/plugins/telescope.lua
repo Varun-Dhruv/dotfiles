b7603855ba6d23a2ec6c7f5961f96c6060af7ea8
Varun Koranne <korannevarun@gmail.com>
2023-08-05 12:55:47 +0530

    Added snippets and basic lsp and mason configuration

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,6 @@
+return {
+    'nvim-telescope/telescope.nvim', -- Telescope core
+    'nvim-telescope/telescope-fzy-native.nvim', -- Fuzzy finder
+
+    dependencies ={'nvim-lua/plenary.nvim'}
+}
---
+++ nvim/lua/snippets/cp.lua
@@ -0,0 +1,41 @@
+local luasnip_status_ok, ls = pcall(require, 'luasnip')
+if not luasnip_status_ok then
+  return
+end
+local snip=ls.snippet
+local text=ls.text_node
+local node=ls.snippet_node
+
+-- custom snippets
+ls.add_snippets(nil,{
+  cpp={
+    snip({
+      trig="include<cp>",
+      namr="CP-Cpp snippet",
+      dscr="Snippet for competitive programming in c++"
+    },{
+      text({
+        "#include<bits/stdc++.h>",
+        "#define ll long long",
+        --`#define endl '\n'`,
+        "#define vi vector<int>",
+        "#define fi first", 
+        "#define se second",
+        "#define pq priority_queue",
+        "#define vll vector<long long>",
+        "#define pb push_back",
+        "#define rep(i, a, b) for (int i = a; i < b; i++)",
+        "using namespace std;",
+        "int main() {",
+        "   ios_base::sync_with_stdio(false);",
+        "   cin.tie(NULL);",
+        "   ll t;",
+        "   cin >> t;",
+        "   while (t--) {",
+        "   }",
+        "   return 0;",
+        "}",
+    })
+    })
+    }
+})
